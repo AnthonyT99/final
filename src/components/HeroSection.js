2561177a3cb46e5a1a5ad422ff9625219d20d079
Tony <anthonysun1@hotmail.com>
2021-07-20 13:11:29 -0400

    added everything

---
+++ src/components/HeroSection.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+//function to display hero section
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+    </div>
+  );
+}
+
+export default HeroSection;
